490e01b4962b431b646c7edeb0f8909cc0deaabc
WiZzard14 <mdridoy144169@gmail.com>
2026-06-26 22:48:47 +0600

    Refactor getAllRecipes function: improve filter logic for category and search parameters

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -5,10 +5,16 @@ export const getAllRecipes = async (req, res) => {
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 6;
     const category = req.query.category;
+    const search = req.query.search;
 
     let filter = {};
+    
     if (category && category !== "All") {
-        filter = { category: category };
+        filter.category = category;
+    }
+    
+    if (search) {
+        filter.recipeName = { $regex: search, $options: 'i' };
     }
 
     const skip = (page - 1) * limit;
@@ -23,6 +29,7 @@ export const getAllRecipes = async (req, res) => {
 
     res.status(200).json({ recipes, totalPages, currentPage: page });
   } catch (error) {
+    console.error("Error fetching recipes:", error);
     res.status(500).json({ message: "Server Error to get recipes" });
   }
 };
